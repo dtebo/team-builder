5f84697d162d32ceb73dea7666483f147ed2a40d
Darren Tebo <darrentebo83@outlook.com>
2020-06-05 19:23:44 -0400

    added the form and started styling it

--- team-builder/src/components/Form/Form.js
+++ team-builder/src/components/Form/Form.js
@@ -5,9 +5,23 @@ const Form = () => {
         <div className='form-wrapper'>
             <form>
                 <label htmlFor='firstName'>
-                    First Name
-                    <input type='text' placeholder />
+                    First Name 
+                    <input type='text' id='firstName' placeholder='Enter First Name' />
                 </label>
+                <label htmlFor='lastName'>
+                    Last Name
+                    <input type='text' placeholder='Enter Last Name' />
+                </label>
+                <label htmlFor='role'>
+                    Role
+                    <select id='role'>
+                        <option value='-'>Select a role</option>
+                        <option value='fe'>Frontend Engineer</option>
+                        <option value='be'>Backend Engineer</option>
+                        <option value='pm'>Project Manager</option>
+                    </select>
+                </label>
+                <button type='submit'>Add Member</button>
             </form>
         </div>
     );
